b00ce80c7218d8d5d99d9a1a3c4504d3e0936d21
Shaddy <shadybett540@gmail.com>
2024-01-17 21:24:33 +0300

    made a class

--- tims.py
+++ tims.py
@@ -3,3 +3,12 @@ from flask_restful import Api,Resource
 
 app =Flask(__name__)
 api = Api(app)
+
+class homePage(Resource):
+    def get():
+        return {'data':'welcome'}
+    
+api.add_resource(homePage)
+
+if __name__=='__main__':
+    app.run(debug=True)
