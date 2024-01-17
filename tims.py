0cff02b8272d86ec5f07bd45905fd467f9e5c9be
Shaddy <shadybett540@gmail.com>
2024-01-17 22:00:08 +0300

    added integer

--- tims.py
+++ tims.py
@@ -5,10 +5,10 @@ app =Flask(__name__)
 api = Api(app)
 
 class HomePage(Resource):
-    def get(self,name):
-        return {'name':'tim'}
+    def get(self,name,age):
+        return {'name':'tim','age':20}
     
-api.add_resource(HomePage,'/homepage/<string:name>')
+api.add_resource(HomePage,'/homepage/<string:name>/<int:age>')
 
 if __name__=='__main__':
     app.run(debug=True,port=5555)
