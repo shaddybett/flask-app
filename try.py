e6502c83b69557740a2a2c9f150ee180203b5b50
Shaddy <shadybett540@gmail.com>
2024-01-17 23:55:56 +0300

    done

--- try.py
+++ try.py
@@ -2,5 +2,5 @@ import requests
 
 Base = 'http://127.0.0.1:5555'
 
-response = requests.put(Base + 'video/1',{'Likes':10})
+response = requests.put(Base + 'video/1',{'likes':10})
 print(response.json())
