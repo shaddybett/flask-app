ab414550d7daa3cd648d3e9f16d59f93db5c3e57
Shaddy <shadybett540@gmail.com>
2024-01-18 02:03:45 +0300

    done

--- tims.py
+++ tims.py
@@ -1,8 +1,13 @@
-from flask import Flask,request
+from flask import Flask
 from flask_restful import Api,Resource,reqparse
 
 app =Flask(__name__)
 api = Api(app)
+
+video_put_args= reqparse.RequestParser()
+video_put_args.add_argument('name',type=str,help='Name of video',required=True)
+video_put_args.add_argument('views',type=int, help='Views required',required=True,)
+video_put_args.add_argument('likes',type=int,help='Likes required',required=True)
 
 videos = {}
 
@@ -11,8 +16,8 @@ class Video(Resource):
         return videos[video_id]
     
     def put(self,video_id):
-        print(request.form)
-        return {}
+        args = video_put_args.parse_args()
+        return {video_id: args}
     
 api.add_resource(Video,'/video/<int:video_id>')
 
--- try.py
+++ try.py
@@ -1,6 +1,6 @@
 import requests
 
-Base = 'http://127.0.0.1:5555'
+Base = 'http://127.0.0.1:5555/'
 
 response = requests.put(Base + 'video/1',{'likes':10})
 print(response.json())
