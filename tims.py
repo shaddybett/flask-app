23d4cffd597213eb0cd292fe0f23c6cba7411f44
Shaddy <shadybett540@gmail.com>
2024-01-17 23:26:35 +0300

    made names variable

--- tims.py
+++ tims.py
@@ -3,12 +3,12 @@ from flask_restful import Api,Resource
 
 app =Flask(__name__)
 api = Api(app)
-
+names = {'Tim':{'age':19,'gender':'male'},'Bett':{'age':18,'gender':'male'}}
 class HomePage(Resource):
-    def get(self,name,age):
-        return {'name':name,'age':age}
+    def get(self,name):
+        return names[name]
     
-api.add_resource(HomePage,'/homepage/<string:name>/<int:age>')
+api.add_resource(HomePage,'/homepage/<string:name>')
 
 if __name__=='__main__':
     app.run(debug=True,port=5555)
--- try.py
+++ try.py
@@ -2,5 +2,5 @@ import requests
 
 Base = 'http://127.0.0.1:5555'
 
-response = requests.get(Base + '/homepage/tim/20')
+response = requests.get(Base + '/homepage/Bett')
 print(response.json())
