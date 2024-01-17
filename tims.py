ae0fd3c5c329ec665db864375089a083f91d9a7b
Shaddy <shadybett540@gmail.com>
2024-01-17 23:36:54 +0300

    changed to videos

--- tims.py
+++ tims.py
@@ -3,12 +3,14 @@ from flask_restful import Api,Resource
 
 app =Flask(__name__)
 api = Api(app)
-names = {'Tim':{'age':19,'gender':'male'},'Bett':{'age':18,'gender':'male'}}
-class HomePage(Resource):
-    def get(self,name):
-        return names[name]
+
+videos = {}
+
+class Video(Resource):
+    def put(self,video_id):
+        return video_id
     
-api.add_resource(HomePage,'/homepage/<string:name>')
+api.add_resource(Video,'/video')
 
 if __name__=='__main__':
     app.run(debug=True,port=5555)
