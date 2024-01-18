92d769f08ee755b85f39cbcecc95ff263de9aa8e
Shaddy <shadybett540@gmail.com>
2024-01-18 07:57:01 +0300

    added headers

--- tims.py
+++ tims.py
@@ -4,10 +4,8 @@ from flask_restful import Api,Resource,reqparse
 app =Flask(__name__)
 api = Api(app)
 
-video_put_args= reqparse.RequestParser()
-video_put_args.add_argument('name',type=str,help='Name of video',required=True)
-video_put_args.add_argument('views',type=int, help='Views required',required=True,)
-video_put_args.add_argument('likes',type=int,help='Likes required',required=True)
+
+
 
 videos = {}
 
@@ -16,10 +14,15 @@ class Video(Resource):
         return videos[video_id]
     
     def put(self,video_id):
+        video_put_args= reqparse.RequestParser()
+        video_put_args.add_argument('name',type=str,help='Name of video',required=True)
+        video_put_args.add_argument('views',type=int, help='Views required',required=True,)
+        video_put_args.add_argument('likes',type=int,help='Likes required',required=True)
+        
         args = video_put_args.parse_args()
         return {video_id: args}
     
 api.add_resource(Video,'/video/<int:video_id>')
 
 if __name__=='__main__':
-    app.run(debug=True,port=5555)
+    app.run(debug=True,port=3000)
