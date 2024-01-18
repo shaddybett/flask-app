aaf5ca22f5507d7aa6cdfe8a0f6204f2a1fe8f63
Shaddy <shadybett540@gmail.com>
2024-01-18 23:16:13 +0300

    added delete method

--- try.py
+++ try.py
@@ -5,8 +5,8 @@ Base = 'http://127.0.0.1:3000/'
 data = {'likes': 10,'name':'tim','views':10000}
 headers ={'Content-Type': 'application/json'}
 
-# response = requests.put(Base + 'video/1',json=data,headers=headers)
-# print(response.json())
-# input()
+response = requests.put(Base + 'video/1',json=data,headers=headers)
+print(response.json())
+input()
 response = requests.get(Base + 'video/1',json=data,headers=headers)
 print(response.json())
